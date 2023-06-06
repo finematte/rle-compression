b698dd852c574b47fa67276941ba82e238eb3a04
anon <mattemill@protonmail.com>
2023-06-06 08:08:50 -0500

    RLE visualization

---
+++ main.py
@@ -0,0 +1,156 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+def rle_encode(img):
+    """
+    Run length encoding.
+    img: source image
+    """
+    pixels = np.array(img).flatten()
+    runs = [] 
+    r = 0  
+    on = pixels[0] 
+
+    for i in range(len(pixels)):
+        if pixels[i] != on:
+            runs.append((r, on))
+            r = 0
+            on = pixels[i]
+        r += 1
+
+    runs.append((r, on))  
+
+    return {'size': img.size, 'runs': runs}
+
+
+def rle_decode(rle_data):
+    """
+    Decode an RLE encoded image.
+    rle_data: RLE data structure
+    """
+    runs = rle_data['runs']
+    shape = rle_data['size']
+    result = np.zeros(np.prod(shape))
+    i = 0
+    for run in runs:
+        result[i:i+run[0]] = run[1]
+        i += run[0]
+
+    return result.reshape(shape[::-1])
+
+def sizeof_fmt(num, suffix='B'):
+    """
+    Convert bytes to human-readable file sizes.
+    """
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f %s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f %s%s" % (num, 'Yi', suffix)
+
+
+def show_images(original, decoded, original_file, decoded_file):
+    """
+    Display the original and decoded images side by side.
+    """
+    fig, axs = plt.subplots(2, 2, figsize=(8, 5), gridspec_kw={'height_ratios': [5, 1]})
+
+    axs[0, 0].imshow(original)
+    axs[0, 0].set_title('Original Image')
+    axs[0, 0].axis('off')
+
+    axs[0, 1].imshow(decoded, cmap='binary')
+    axs[0, 1].set_title('Decoded Image')
+    axs[0, 1].axis('off')
+
+    original_size = os.path.getsize(original_file)
+    decoded_size = os.path.getsize(decoded_file)
+
+    axs[1, 0].axis('off')
+    axs[1, 0].text(0.5, 0.5, f"Original Image Size: {sizeof_fmt(original_size)}", ha='center', va='center')
+
+    axs[1, 1].axis('off')
+    axs[1, 1].text(0.5, 0.5, f"Decoded Image Size: {sizeof_fmt(decoded_size)}", ha='center', va='center')
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+def compare_file_sizes(original_file, decoded_file):
+    """
+    Compare the file sizes of the original and decoded images.
+    """
+    original_size = os.path.getsize(original_file)
+    decoded_size = os.path.getsize(decoded_file)
+
+    print(f"Original Image Size: {original_size} bytes")
+    print(f"Decoded Image Size: {decoded_size} bytes")
+
+
+def select_image():
+    """
+    Open a file dialog to select an image file.
+    """
+    image_path = filedialog.askopenfilename(filetypes=(("Image files", "*.bmp*"), ("All files", "*.*")))
+    if image_path:
+        process_image(image_path)
+
+
+def process_image(image_path):
+    """
+    Process the selected image.
+    """
+    try:
+        img = Image.open(image_path)
+
+        encoded = rle_encode(img.convert('L'))
+        print(f"RLE: {encoded}")
+
+
+        decoded_img_data = rle_decode(encoded)
+
+        original_img_data = np.array(img)
+
+        decoded_img = Image.fromarray(np.uint8(decoded_img_data * 255))
+        decoded_file = "decoded.bmp"
+        decoded_img.save(decoded_file)
+        print(f"Saved decoded image as {decoded_file}")
+
+        show_images(original_img_data, decoded_img_data, image_path, decoded_file)
+
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+
+
+def create_gui():
+    """
+    Create the graphical user interface.
+    """
+    root = tk.Tk()
+    root.title("Fax Coding Demo")
+
+    root.geometry("150x150")
+
+    btn_select = tk.Button(root, text="Select Image", command=select_image)
+
+    window_width = 150
+    window_height = 150
+    button_width = btn_select.winfo_reqwidth()
+    button_height = btn_select.winfo_reqheight()
+    position_top = int(window_height / 2 - button_height / 2)
+    position_right = int(window_width / 2 - button_width / 2)
+
+    btn_select.place(x=position_right, y=position_top)
+
+    root.mainloop()
+
+
+
+if __name__ == "__main__":
+    create_gui()
